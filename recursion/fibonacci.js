499df9b4d5ca3e35bdd177a460c888e966fe5777
Gohar <gohar.mamunts@gmail.com>
2022-06-07 20:24:29 +0400

    fibonacci

---
+++ recursion/fibonacci.js
@@ -0,0 +1,16 @@
+// calculate the 40th number of fibonacci sequence
+
+function fib(n, memo = {}) {
+  if (n in memo) {
+    return memo[n];
+  }
+
+  if (n <= 2) {
+    return 1;
+  }
+
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
+
+console.log(fib(40));
